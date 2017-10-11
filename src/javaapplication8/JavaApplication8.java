4ceed538b8d8328d82724d15a56c1bf29f5a4d73
menzelths <menzelths@gmail.com>
2017-10-11 21:51:20 +0200

    initial

---
+++ src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author menze
+ */
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int[] summe=new int[6];
+        for (int i=0;i<100;i++){
+            int zz=(int)(Math.random()*6+1);
+            summe[zz-1]++;
+        }
+        
+        for (int i=0;i<summe.length;i++){
+            System.out.println(summe[i]);
+            int c=54;
+        }
+            
+    }
+    
+}
